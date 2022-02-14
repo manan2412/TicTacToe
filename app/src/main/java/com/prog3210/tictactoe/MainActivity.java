bddd51db5d54f9c53a3447a5b210ceb43e651853
Manan Patel <64778754+manan2412@users.noreply.github.com>
2022-02-14 03:21:20 -0600

    First Commit

---
+++ app/src/main/java/com/prog3210/tictactoe/MainActivity.java
@@ -0,0 +1,19 @@
+package com.prog3210.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.logo);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
